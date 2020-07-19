470edc4f595b9c53ca80ec51749aa8f232b3e660
Cloud Strife <cloudstrife2612@gmail.com>
2020-07-19 11:27:02 +0300

    add redirect post

--- djangoapp/djangoappcore/settings.py
+++ djangoapp/djangoappcore/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'NAME': 'djangoapp',
         'USER': 'postgres',
         'PASSWORD': 'admin',
-        'HOST':'localhost',
+        'HOST': 'localhost',
         'PORT': '5432'
     }
 }
@@ -116,6 +116,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATICFILES_DIRS = [
+
+    os.path.join(BASE_DIR, 'djangoappcore/static/')
+
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
